8a27362ba1893fbc51f6d105fb688b2dfd48271b
ElphieGG <jazzlynpancho18@gmail.com>
2025-03-28 13:55:34 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-
 import Header from "./components/header"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Hero from "./components/hero"
+import About from "./components/about"
 
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
